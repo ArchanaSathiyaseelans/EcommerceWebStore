c8a60ffd4f32e8b029e1ba712403dd2018b6bdae
ArchanaSathiyaseelans <archanasathiyaseelans26@gmail.com>
2026-05-07 04:43:09 +0530

    implement Clerk authentication and enivronment configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/react";
 
-createRoot(document.getElementById('root')).render(
+const clerkFrontendApi = "VITE_CLERK_PUBLISHABLE_KEY";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ClerkProvider frontendApi={clerkFrontendApi}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
-)
+);
